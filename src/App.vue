2f9a9625eb4f19f8adf3b3fbabc7c857fb20949f
Antoine DA SILVA <antoine.da-silva@maincare.fr>
2021-08-13 14:51:34 +0200

    Add default theme: system

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-app>
+  <v-app :theme="theme">
     <v-app-bar app>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
@@ -9,6 +9,35 @@
     </v-main>
   </v-app>
 </template>
+
+<script lang="ts">
+import { Vue } from "vue-property-decorator";
+export default class App extends Vue {
+  theme = "light";
+
+  setTheme(e: MediaQueryList | MediaQueryListEvent): void {
+    if (e.matches) {
+      this.theme = "dark";
+    } else {
+      this.theme = "light";
+    }
+  }
+
+  initTheme(): void {
+    const darkThemeMq = window.matchMedia("(prefers-color-scheme: dark)");
+    this.setTheme(darkThemeMq);
+
+    const updateTheme = this.setTheme.bind(this);
+    darkThemeMq.addEventListener("change", (e) => {
+      updateTheme(e);
+    });
+  }
+
+  created(): void {
+    this.initTheme();
+  }
+}
+</script>
 
 <style lang="scss">
 .v-app-bar {
