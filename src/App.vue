613f3d0c355693eeba7b5f773266e0bd6ced7a68
Antoine DA SILVA <antoine.da-silva@maincare.fr>
2021-08-13 12:32:54 +0200

    Add Vuetify 3.0-alpha

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,27 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view />
+  <v-app>
+    <v-app-bar app>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </v-app-bar>
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
+.v-app-bar {
+  padding: 0 25px;
 
   a {
     font-weight: bold;
     color: #2c3e50;
+    text-decoration: none;
+    margin: 0 5px;
 
     &.router-link-exact-active {
-      color: #42b983;
+      color: #1697f6;
     }
   }
 }
